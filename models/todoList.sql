db63200733002f17bfeb01bd4e06ec92f00df8bb
twishasaraiya <saraiyatwisha@gmail.com>
2019-05-10 10:06:39 +0530

    feat: update todolist schema

--- models/todoList.sql
+++ models/todoList.sql
@@ -1,8 +1,9 @@
 create table todoList(
-    -> id int not null auto_increment,
-    -> text varchar(500) not null,
-    -> complete int default 0 not null,
-    -> user_id int references users(id),
-    -> created_date date not null,
-    -> primary key(id)
+   id int not null auto_increment,
+   text text not null,
+   complete int default 0 not null,
+   user_id int references users(id),
+   created_date date not null,
+   color varchar(10) default '#fff',
+   primary key(id)
     -> );
